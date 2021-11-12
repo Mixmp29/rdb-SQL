5a1ac3b9a45c6e9244740a51ec4a898263c20444
Mixmp29 <denisov.mihail2013@yandex.ru>
2021-11-12 18:20:35 +0700

    Create structure Location

--- src/sql/Location.hpp
+++ src/sql/Location.hpp
@@ -3,7 +3,12 @@
 #include <cstddef>
 
 namespace rdb::sql {
-
-struct Location {
   
+struct Location {
+  Location(size_t row, size_t col, size_t offset) : row_(row), col_(col), offset_(offset) {}
+  size_t row_;
+  size_t col_;
+  size_t offset_;
+};
+
 }
